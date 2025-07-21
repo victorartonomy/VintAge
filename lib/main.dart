6f23b611fb021c643025781faa09a2f1c48f15ea
Shelton Coutinho <shelvic10@gmail.com>
2025-07-21 12:22:13 +0530

    Temporarily removed the AppCheck

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:vintage/config/firebase_options.dart';
@@ -10,7 +9,7 @@ void main() async {
   // firebase init
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  await FirebaseAppCheck.instance.activate(androidProvider: AndroidProvider.debug);
+  //await FirebaseAppCheck.instance.activate(androidProvider: AndroidProvider.debug);
 
   // run app
   runApp(MyApp());
