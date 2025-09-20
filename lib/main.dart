37c793b8fce7f4948df793abe2811bcacafcbed5
Shelton Coutinho <shelvic10@gmail.com>
2025-09-20 18:53:52 +0530

    added AppCheck

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:vintage/config/firebase_options.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 
 import 'app.dart';
 
@@ -9,7 +10,11 @@ void main() async {
   // firebase init
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  //await FirebaseAppCheck.instance.activate(androidProvider: AndroidProvider.debug);
+  await FirebaseAppCheck.instance.activate(
+    // androidProvider: AndroidProvider.debug,
+    androidProvider: AndroidProvider.playIntegrity,
+    appleProvider: AppleProvider.deviceCheck
+  );
 
   // run app
   runApp(MyApp());
